66afafa438dd70af13c0ed0acdf6b236521037b1
eGluZl <im.hxy@outlook.com>
2022-01-13 14:07:31 +0800

    测试接口获取

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,17 @@
     ha ha ha
   </div>
 </template>
+
+<script setup>
+import { testAPI } from '~~/server/api/common'
+
+const { data } = await useFetch(testAPI, {
+  method: 'POST',
+  params: {
+    holder: '233',
+  },
+})
+
+console.log(data.value)
+
+</script>
---
+++ server/api/common.ts
@@ -0,0 +1,3 @@
+export const bingImg = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+
+export const testAPI = 'http://localhost/gehirn-api/api/module/fate/order'
